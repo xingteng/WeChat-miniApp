4158d558c84e9f6965d4c115e06d0eb01ef4fe3e
xingteng <xingteng2018@163.com>
2018-08-17 20:59:32 +0800

    添加onLoad()及获取JSON数据

--- weather/pages/index/index.js
+++ weather/pages/index/index.js
@@ -1,3 +1,16 @@
 //index.js
 //获取应用实例
-Page({})
+Page({
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', //接口地址
+      data: {
+        city: '广州市'
+      },
+     
+      success: res => {
+        console.log(res)
+      }
+    })
+  }
+})
